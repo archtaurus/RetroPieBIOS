d66fd5d31aa33f67a882ca30f95a5b73c1cc0f2c
Zhao Xin <7176466@qq.com>
2021-04-18 10:58:14 +0800

    update

--- md5sum.test.js
+++ md5sum.test.js
@@ -14,19 +14,16 @@ const md5 = md5sum(filePath)
 
 const data = fs.readFileSync(filePath).toString()
 const lines = data.split('\n')
-lines.forEach(line => {
+lines.forEach((line) => {
     const match = line.match(/rom \( name (.+) size (\d+) crc (.+) md5 (.+) sha1 (.+) \)/)
     if (match) allBIOS.push({ filename: match[1].replace(/\"/g, ''), md5: match[4] })
 })
 
 describe('all files md5sum check', () => {
-    test(`md5sum("System.dat") should be "f608bbe045dcfa5b8e4f4bbb9ed8070d"`, () => {
-        expect(md5).toEqual('f608bbe045dcfa5b8e4f4bbb9ed8070d')
-    })
-    allBIOS.forEach(bios => {
+    allBIOS.forEach((bios) => {
         const { filename, md5 } = bios
         test(`md5sum("${filename}") should be "${md5}"`, () => {
-            const filepath = path.join(__dirname, '..', 'BIOS', filename)
+            const filepath = path.join(__dirname, 'BIOS', filename)
             expect(md5sum(filepath)).toEqual(md5)
         })
     })
