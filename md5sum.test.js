9287db8c6745892398e4dcb7c1f7fc84f17e1349
Zhao Xin <7176466@qq.com>
2023-06-15 15:05:00 +0800

    add 9 files md5sum

--- md5sum.test.js
+++ md5sum.test.js
@@ -1,30 +1,41 @@
-const fs = require('fs')
-const path = require('path')
-const crypto = require('crypto')
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
 
 function md5sum(filePath) {
-    const hash = crypto.createHash('md5')
-    const data = fs.readFileSync(filePath)
-    hash.update(data, 'utf8')
-    return hash.digest('hex')
+    const hash = crypto.createHash("md5");
+    const data = fs.readFileSync(filePath);
+    hash.update(data, "utf8");
+    return hash.digest("hex");
 }
-const allBIOS = []
-const filePath = path.join(__dirname, 'System.dat')
-const md5 = md5sum(filePath)
+const allBIOS = [];
+const filePath1 = path.join(__dirname, "System.dat");
+const data1 = fs.readFileSync(filePath1).toString();
+const lines1 = data1.split("\n");
+lines1.forEach((line) => {
+    const match = line.match(
+        /rom \( name (.+) size (\d+) crc (.+) md5 (.+) sha1 (.+) \)/
+    );
+    if (match)
+        allBIOS.push({ filename: match[1].replace(/\"/g, ""), md5: match[4] });
+});
+const filePath2 = path.join(__dirname, "NotinSystem.dat");
+const data2 = fs.readFileSync(filePath2).toString();
+const lines2 = data2.split("\n");
+lines2.forEach((line) => {
+    const match = line.match(
+        /rom \( name (.+) size (\d+) crc (.+) md5 (.+) sha1 (.+) \)/
+    );
+    if (match)
+        allBIOS.push({ filename: match[1].replace(/\"/g, ""), md5: match[4] });
+});
 
-const data = fs.readFileSync(filePath).toString()
-const lines = data.split('\n')
-lines.forEach((line) => {
-    const match = line.match(/rom \( name (.+) size (\d+) crc (.+) md5 (.+) sha1 (.+) \)/)
-    if (match) allBIOS.push({ filename: match[1].replace(/\"/g, ''), md5: match[4] })
-})
-
-describe('all files md5sum check', () => {
+describe("all files md5sum check", () => {
     allBIOS.forEach((bios) => {
-        const { filename, md5 } = bios
+        const { filename, md5 } = bios;
         test(`md5sum("${filename}") should be "${md5}"`, () => {
-            const filepath = path.join(__dirname, 'BIOS', filename)
-            expect(md5sum(filepath)).toEqual(md5)
-        })
-    })
-})
+            const filepath = path.join(__dirname, "BIOS", filename);
+            expect(md5sum(filepath)).toEqual(md5);
+        });
+    });
+});
